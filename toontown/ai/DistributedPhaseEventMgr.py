2955703426a2cd2ba432fe804aa179e40756da5b
Gumball9090 <146196286+Gumball9090@users.noreply.github.com>
2023-09-27 21:43:34 -0300

    Update DistributedPhaseEventMgr.py

--- toontown/ai/DistributedPhaseEventMgr.py
+++ toontown/ai/DistributedPhaseEventMgr.py
@@ -1,5 +1,3 @@
-# File: D (Python 2.4)
-
 from direct.directnotify import DirectNotifyGlobal
 from direct.distributed import DistributedObject
 import datetime
@@ -10,35 +8,25 @@ class DistributedPhaseEventMgr(DistributedObject.DistributedObject):
     def __init__(self, cr):
         DistributedObject.DistributedObject.__init__(self, cr)
         self.holidayDates = []
-
     
     def setIsRunning(self, isRunning):
         self.isRunning = isRunning
-
     
     def setNumPhases(self, numPhases):
         self.numPhases = numPhases
-
     
     def setCurPhase(self, curPhase):
         self.curPhase = curPhase
-
     
     def getIsRunning(self):
         return self.isRunning
 
-    
     def getNumPhases(self):
         return self.numPhases
 
-    
     def getCurPhase(self):
         return self.curPhase
 
-    
     def setDates(self, holidayDates):
         for holidayDate in holidayDates:
             self.holidayDates.append(datetime.datetime(holidayDate[0], holidayDate[1], holidayDate[2], holidayDate[3], holidayDate[4], holidayDate[5]))
-        
-
-
