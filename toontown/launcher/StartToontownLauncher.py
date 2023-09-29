b1f9038848eca86b1aea5838d6f5c891a9baa5c1
Vitor25laser <97393601+Vitor23laser@users.noreply.github.com>
2023-09-29 19:31:42 -0300

    Add files via upload

---
+++ toontown/launcher/StartToontownLauncher.py
@@ -0,0 +1,8 @@
+# File: S (Python 2.4)
+
+import sys
+sys.path = [
+    '']
+from toontown.launcher.ToontownLauncher import ToontownLauncher
+launcher = ToontownLauncher()
+launcher.notify.info('Reached end of StartToontownLauncher.py.')
