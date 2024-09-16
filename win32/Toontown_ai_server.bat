6875af7f782d2ae0f3603337f4fb89d1d39aa62f
Vitor26laser <97393601+Vitor23laser@users.noreply.github.com>
2024-09-16 07:49:14 -0300

    Update Toontown_ai_server.bat

--- win32/Toontown_ai_server.bat
+++ win32/Toontown_ai_server.bat
@@ -6,7 +6,7 @@ rem Read the contents of PPYTHON_PATH into %PPYTHON_PATH%:
 set /P PPYTHON_PATH= p3d/python/ppython.exe
 
 :main
-PPYTHON_PATH -m toontown.ai.AIStart --base-channel 401000000 ^
+%PPYTHON_PATH% -m toontown.ai.AIStart --base-channel 401000000 ^
                --max-channels 999999 --stateserver 4002 ^
                --messagedirector-ip 127.0.0.1:7199 ^
                --eventlogger-ip 127.0.0.1:7197 ^
