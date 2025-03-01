26748fd3b3c258a223ce0fbd5b0cbd1d4da3ac83
Vitor26laser <97393601+Vitor23laser@users.noreply.github.com>
2025-03-01 12:02:40 -0300

    Update toontownLauncher.bat

--- win32/toontownLauncher.bat
+++ win32/toontownLauncher.bat
@@ -2,10 +2,7 @@
 title Toontown_BR - Game Client
 cd..
 
-rem Read the contents of PPYTHON_PATH into %PPYTHON_PATH%:
-set /P PPYTHON_PATH=<PPYTHON_PATH
-
 set LOGIN_TOKEN=dev
 
-%PPYTHON_PATH% -m toontown.launcher.StartToontownLauncher
+p3d\python\ppython.exe -m toontown.launcher.StartToontownLauncher
 pause
